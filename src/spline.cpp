9e85bbe9805d14257c8dca035f3dae143a369829
evco3 <evco3@hotmail.com>
2024-03-17 16:41:32 -0400

    Initial commit

---
+++ src/spline.cpp
@@ -0,0 +1,339 @@
+/* spline.cpp
+ */
+
+
+#include "spline.h"
+#include "main.h"
+#include "linalg.h"
+
+
+#define DIVS_PER_SEG 20         // number of samples of on each spline segment (for arc length parameterization)
+
+float Spline::M[][4][4] = {
+
+  { { 0, 0, 0, 0},              // Linear
+    { 0, 0, 0, 0},
+    { 0,-1, 1, 0},
+    { 0, 1, 0, 0}, },
+
+  { { -0.5,  1.5, -1.5,  0.5 }, // Catmull-Rom
+    {  1.0, -2.5,  2.0, -0.5 },
+    { -0.5,  0.0,  0.5,  0.0 },
+    {  0.0,  1.0,  0.0,  0.0 } },
+
+  { { -0.1667, 0.5,   -0.5,    0.1667 }, // B-spline
+    {  0.5,   -1.0,    0.5,    0.0    },
+    { -0.5,    0.0,    0.5,    0.0    },
+    {  0.1667, 0.6667, 0.1667, 0.0    } }
+  
+};
+
+
+const char * Spline::MName[] = {
+  "linear",
+  "Catmull-Rom",
+  "B-spline",
+  ""
+};
+
+
+// Evaluate the spline at parameter 't'.  Return the value, tangent
+// (i.e. first derivative), or binormal, depending on the
+// 'returnDerivative' parameter.
+// 
+// The spline is continuous, so the first data point appears again
+// after the last data point.  t=0 at the first data point and t=n-1
+// at the n^th data point.  For t outside this range, use 't modulo n'.
+//
+// Use the change-of-basis matrix in M[currSpline].
+
+// implement an operator to multiple a vec3 by a float element-wise
+vec3 operator*(vec3 v, float f) {
+  return vec3(v.x*f, v.y*f, v.z*f);
+}
+
+vec3 Spline::eval( float t, evalType type )
+
+{
+  // YOUR CODE HERE
+  if (t < 0)
+    t += data.size();
+  else if (t >= data.size())
+    t -= data.size();
+
+  vec3 q0 = data[((int(t) - 1) + data.size()) % data.size()];
+  vec3 q1 = data[int(t) % data.size()];
+  vec3 q2 = data[(int(t) + 1) % data.size()];
+  vec3 q3 = data[(int(t) + 2) % data.size()];
+
+  if (t >= data.size() - 1) {
+    q3 = data[0];
+  }
+
+  // Calculate Mv matrix
+  
+  vec3 v[4] = { q0, q1, q2, q3 };
+  vec3 Mv[4] = { vec3(0,0,0), vec3(0,0,0), vec3(0,0,0), vec3(0,0,0) }; 
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      Mv[i] = Mv[i] + M[currSpline][i][j] * v[j];
+    }
+  }
+  float u = t - floor(t);
+  
+
+  
+  if (type == VALUE) {
+    return u*u*u*Mv[0] + u*u*Mv[1] + u*Mv[2] + Mv[3];
+  } else if (type == TANGENT) {
+    return 3*u*u*Mv[0] + 2*u*Mv[1] + Mv[2];
+  } else {
+    return vec3(0,0,0);
+  }
+}
+
+
+// Find a local coordinate system at t.  Return the axes x,y,z.  y
+// should point as much up as possible and z should point in the
+// direction of increasing position on the curve.
+
+
+void Spline::findLocalSystem( float t, vec3 &o, vec3 &x, vec3 &y, vec3 &z )
+
+{
+#if 1
+  // YOUR CODE HERE
+  o = eval(t, VALUE);
+
+
+  z = eval(t, TANGENT);
+  z = z.normalize();
+
+  x = z ^ vec3(0, 0, 1);
+  x = x.normalize();
+
+  y = x ^ z;
+  y = y.normalize();
+
+#else
+  
+  o = vec3(0,0,0);
+  x = vec3(1,0,0);
+  y = vec3(0,1,0);
+  z = vec3(0,0,1);
+
+#endif
+}
+
+
+mat4 Spline::findLocalTransform( float t )
+
+{
+  vec3 o, x, y, z;
+
+  findLocalSystem( t, o, x, y, z );
+
+  mat4 M;
+  M.rows[0] = vec4( x.x, y.x, z.x, o.x );
+  M.rows[1] = vec4( x.y, y.y, z.y, o.y );
+  M.rows[2] = vec4( x.z, y.z, z.z, o.z );
+  M.rows[3] = vec4( 0, 0, 0, 1 );
+
+  return M;
+}
+
+
+// Draw a point and a local coordinate system for parameter t
+
+
+void Spline::drawLocalSystem( float t, mat4 &MVP )
+
+{
+  vec3 o, x, y, z;
+
+  findLocalSystem( t, o, x, y, z );
+
+  mat4 M;
+  M.rows[0] = vec4( x.x, y.x, z.x, o.x );
+  M.rows[1] = vec4( x.y, y.y, z.y, o.y );
+  M.rows[2] = vec4( x.z, y.z, z.z, o.z );
+  M.rows[3] = vec4( 0, 0, 0, 1 );
+
+  M = MVP * M * scale(6,6,6);
+  axes->draw(M);
+}
+
+
+// Draw the spline with even parameter spacing
+
+
+void Spline::draw( mat4 &MV, mat4 &MVP, vec3 lightDir, bool drawIntervals )
+
+{
+  // Draw the spline
+  
+  vec3 *points = new vec3[ data.size()*DIVS_PER_SEG + 1 ];
+  vec3 *colours = new vec3[ data.size()*DIVS_PER_SEG + 1 ];
+
+  int i = 0;
+  for (float t=0; t<data.size()-1; t+=1/(float)DIVS_PER_SEG) {
+    points[i] = value( t );
+    colours[i] = SPLINE_COLOUR;
+    i++;
+  }
+
+  segs->drawSegs( GL_LINE_LOOP, points, colours, i, MV, MVP, lightDir );
+
+  // Draw points evenly spaced in the parameter
+
+  if (drawIntervals)
+    for (float t=0; t<data.size(); t+=1/(float)DIVS_PER_SEG)
+      drawLocalSystem( t, MVP );
+
+  delete[] points;
+  delete[] colours;
+}
+
+
+// Draw the spline with even arc-length spacing
+
+
+void Spline::drawWithArcLength( mat4 &MV, mat4 &MVP, vec3 lightDir, bool drawIntervals )
+
+{
+  // Draw the spline
+  
+  vec3 *points = new vec3[ data.size()*DIVS_PER_SEG + 1 ];
+  vec3 *colours =  new vec3[ data.size()*DIVS_PER_SEG + 1 ];
+
+  int i = 0;
+  for (float t=0; t<data.size(); t+=1/(float)DIVS_PER_SEG) {
+    points[i] = value( t );
+    colours[i] = SPLINE_COLOUR;
+    i++;
+  }
+
+  segs->drawSegs( GL_LINE_LOOP, points, colours, i, MV, MVP, lightDir );
+
+  // Draw points evenly spaced in arc length
+
+  if (drawIntervals) {
+    
+    float totalLength = totalArcLength();
+
+    for (float s=0; s<totalLength; s+=totalLength/(float)(data.size()*DIVS_PER_SEG)) {
+      float t = paramAtArcLength( s );
+      drawLocalSystem( t, MVP );
+    }
+  }
+
+  delete[] points;
+  delete[] colours;
+}
+
+
+// Fill in an arcLength array such that arcLength[i] is the estimated
+// arc length up to the i^th sample (using DIVS_PER_SEG samples per
+// spline segment).
+
+
+void Spline::computeArcLengthParameterization()
+
+{
+  if (data.size() == 0)
+    return;
+
+  if (arcLength != NULL)
+    delete [] arcLength;
+
+  arcLength = new float[ data.size() * DIVS_PER_SEG + 1 ];
+
+  // first sample at length 0
+
+  arcLength[0] = 0;
+  vec3 prev = value(0);
+
+  maxHeight = prev.z;
+
+  // Compute intermediate lengths
+
+  int k = 1;
+  
+  for (int i=0; i<data.size(); i++)
+    for (int j=(i>0?0:1); j<DIVS_PER_SEG; j++) {
+
+      vec3 next = value( i + j/(float)DIVS_PER_SEG );
+
+      arcLength[k] = arcLength[k-1] + (next-prev).length();
+      prev = next;
+      k++;
+
+      if (next.z > maxHeight)
+        maxHeight = next.z;
+    }
+
+  // last sample at full length
+
+  vec3 next = value( data.size() );
+  arcLength[k] = arcLength[k-1] + (next-prev).length();
+
+  if (next.z > maxHeight)
+    maxHeight = next.z;
+  
+  mustRecomputeArcLength = false;
+}
+
+
+// Find the spline parameter at a particular arc length, s.
+
+
+float Spline::paramAtArcLength( float s )
+
+{
+  if (mustRecomputeArcLength)
+    computeArcLengthParameterization();
+
+  // Do binary search on arc lengths to find l such that 
+  //
+  //        arcLength[l] <= s < arcLength[l+1].
+
+  if (s < 0)
+    s += arcLength[ data.size() * DIVS_PER_SEG ];
+
+  int l = 0;
+  int r = data.size()*DIVS_PER_SEG;
+
+  while (r-l > 1) {
+    int m = (l+r)/2;
+    if (arcLength[m] <= s)
+      l = m;
+    else
+      r = m;
+  }
+
+  if (arcLength[l] > s || arcLength[l+1] <= s)
+    return (l + 0.5) / (float) DIVS_PER_SEG;
+  
+  // Do linear interpolation in arcLength[l] ... arcLength[l+1] to
+  // find position of s.
+
+  float p = (s - arcLength[l]) / (arcLength[l+1] - arcLength[l]);
+
+  // Return the curve parameter at s
+
+  return (l+p) / (float) DIVS_PER_SEG;
+}
+
+
+
+float Spline::totalArcLength()
+
+{
+  if (data.size() == 0)
+    return 0;
+
+  if (mustRecomputeArcLength)
+    computeArcLengthParameterization();
+
+  return arcLength[ data.size() * DIVS_PER_SEG ];
+}
---
+++ src/train.cpp
@@ -0,0 +1,128 @@
+// train.cpp
+
+#include "train.h"
+#include "main.h"
+#include "linalg.h"
+#include "spline.h"
+
+
+#define SPHERE_RADIUS 5.0
+#define SPHERE_COLOUR 238/255.0, 106/255.0, 20/255.0
+
+float velocity = 70;
+float acceleration;
+
+// Draw the train.
+//
+// 'flag' is toggled by pressing 'F' and can be used for debugging
+
+ 
+void Train::draw( mat4 &WCStoVCS, mat4 &WCStoCCS, vec3 lightDir, bool flag )
+
+{
+#if 1
+
+  // YOUR CODE HERE
+  float t = spline->paramAtArcLength( pos );
+  
+
+  // Draw cube
+  vec3 o, x, y, z;
+  spline->findLocalSystem( t, o, x, y, z );
+
+  vec3 axis = vec3(1,0,0) ^ z;
+  float theta = atan2( axis.length(), vec3(1,0,0)*z );
+
+  mat4 R;
+  R.rows[0] = vec4(x, 0);
+  R.rows[1] = vec4(y, 0);
+  R.rows[2] = vec4(z, 0);
+  R.rows[3] = vec4(0, 0, 0, 1);
+
+
+  mat4 M   = translate( o ) *  scale( 10, 10, 10 );
+  mat4 MV  = WCStoVCS * M;
+  mat4 MVP = WCStoCCS * M;
+
+  cube->draw( MV, MVP, lightDir, vec3( SPHERE_COLOUR ) );
+
+#else
+  
+ 
+
+#endif
+}
+
+//function to do dot product vetween to vec3s
+float dot(vec3 a, vec3 b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+  //function that finds the magnitude of downard z direction at local position
+  float magnitudeAtPos(float pos, Spline* spline) {
+  float t = spline->paramAtArcLength( pos );
+  
+  vec3 z = spline->eval(t, TANGENT).normalize();
+
+  vec3 localZ = z;
+  vec3 worldZ = vec3(0, 0, -1);
+
+  float projection = dot(localZ, worldZ);
+
+  return projection;
+}
+
+void Train::advance( float elapsedSeconds )
+{
+#if 1
+
+  // YOUR CODE HERE
+  float arcLength = spline->totalArcLength();
+
+  //check projection of trains' local x onto world z to calculate the angle its facing and adjust speed based on magnitude of that projection
+  float magSum = 0;
+  for (int i = 0; i < 5; i++) {
+    float offset = float(i - 2);
+    float currentPos = fmod(pos + arcLength + offset, arcLength);
+    magSum += magnitudeAtPos(currentPos, spline);
+  }
+
+  //get magnitude at local position
+  float magnitude = magSum / 5;
+
+
+  // float angle = anglesSum / 5;
+
+  //update speed based on the magnitude of the angle
+  acceleration = magnitude * 0.8;
+
+  if (velocity + acceleration < 35) {
+    velocity = 35;
+  } else if (velocity + acceleration > 175) {
+    velocity = 175;
+  } else {
+    velocity += acceleration;
+  }
+  
+
+  //update the position of the train
+
+  pos += velocity * elapsedSeconds;
+
+  cout << "magnitude:" << magnitude << endl;
+  cout << "Velocity:" << velocity << endl;
+  cout << "Acceleration:" << acceleration << endl;
+
+
+  if(pos > arcLength) {
+    pos = 0;
+  }
+
+
+#else
+
+ 
+  
+
+#endif
+}
